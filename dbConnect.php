f90975af8d658b0a64ff732bfcdcaa1efa820726
anhquan202 <111556330+anhquan202@users.noreply.github.com>
2024-02-01 15:05:16 +0700

    Update dbConnect.php

--- dbConnect.php
+++ dbConnect.php
@@ -3,10 +3,6 @@ $servername = "bgksslb6ibhcfqyrtfub-mysql.services.clever-cloud.com";
 $username = "uxukqqoyfecuh9mn";
 $password = "y6no8mGVItMjryASVtvO";
 $dbname = "bgksslb6ibhcfqyrtfub";
-// $servername = "localhost";
-// $username = "hiep";
-// $password = "1530";
-// $dbname = "qlbanhang";
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
 ?>
